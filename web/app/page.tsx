23f2a55ed043d2a4a7cf66e8f51d3a1697e278a8
manweljs <manweljs@gmail.com>
2024-04-03 15:50:57 +0800

    update base map

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,83 +1,113 @@
 "use client";
-import { Environment, Lightformer, MeshReflectorMaterial, OrbitControls, PerspectiveCamera, Plane, SpotLight, Stars } from "@react-three/drei";
-import { Canvas, Vector3, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { Environment, Text3D, OrbitControls, PerspectiveCamera, Plane, SpotLight, Stars, OrthographicCamera, ContactShadows, RoundedBox } from "@react-three/drei";
+import { Canvas, Vector3, useFrame, useThree } from "@react-three/fiber";
+import { Suspense, useEffect, useRef } from "react";
 import style from "../styles/style.module.sass"
 import { degreeToRadian } from "utils";
 import * as THREE from "three";
+import { Car } from "../src/components/models/Car";
+import { Husky } from "@/components/models/Husky";
+import { Physics, RigidBody } from "@react-three/rapier";
+import { TransformControls } from "three-stdlib";
+import { Island } from "@/components/models/Island";
+import { BlueSky } from "@/components/models/BlueSky";
+
+const baseColor = "#8dc2ff"
+const primaryColor = "#0062ff"
+
+function CameraController() {
+  const { camera } = useThree();
+
+  useEffect(() => {
+    camera.position.set(75, 10, 30); // Mengatur posisi kamera
+    camera.updateProjectionMatrix(); // Memperbarui matriks proyeksi kamera setelah mengubah properti
+  }, [camera]);
+
+  return null;
+}
+
 
 export default function Page() {
   return (
     <div className={style.home}>
-      <Canvas shadows>
-        {/* <ambientLight /> */}
-        <OrbitControls />
-        <PerspectiveCamera makeDefault fov={100} position={[6, 2, 6]} />
+      <Suspense>
+        <Canvas shadows>
+          <CameraController />
+          <OrbitControls maxPolarAngle={1.2} />
+          <PerspectiveCamera makeDefault position={[-5, 30, 45]} fov={45} />
 
-        {/* <directionalLight color={"white"} position={[2, 2, 2]} intensity={.7} /> */}
-        <SpotLight
-          position={[0, 15, 0]}
-          distance={20}
-          angle={0.5}
-          attenuation={1}
-          penumbra={1}
-          decay={.1}
-          intensity={.5}
-          color={"#ffffff"}
-        />
+          <directionalLight
 
-        <Cube position={[0, 5, 0]} />
+            castShadow
+            position={[5, 15, 5]}
+            intensity={1.5}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-near={0.2}
+            shadow-camera-far={500}
+            shadow-camera-left={-50}
+            shadow-camera-right={50}
+            shadow-camera-top={50}
+            shadow-camera-bottom={-50}
+          />
 
-        <BasePlane />
+          {/* <directionalLight
+            castShadow
+            position={[0, 10, 0]}
+            intensity={2}
+            shadow-mapSize={[1024, 1024]}
+            color={"#bcdbff"}
+          /> */}
 
-        <Environment background  >
-          <mesh>
-            <sphereGeometry args={[50, 100, 100]} />
-            <meshBasicMaterial color={"#55bffc"} side={THREE.BackSide} />
-          </mesh>
-        </Environment>
+          {/* <spotLight
+            position={[0, 10, 0]}
+            intensity={300}
+            angle={180}
+            penumbra={1}
+            castShadow
+            color={"#bcdbff"} /> */}
 
-      </Canvas>
+          <BlueSky />
+
+
+          {/* <Car scale={6} position={[0, 8, 0]} /> */}
+          {/* <Physics gravity={[0, -5, 0]} colliders="hull"  >
+
+            <RigidBody colliders={"hull"} >
+              <Husky scale={.5} />
+            </RigidBody>
+
+
+            <RigidBody type="fixed"  >
+              <Island scale={1} position={[-2, -1, 6]} rotation={[0, .9, 0]} />
+            </RigidBody>
+
+
+
+          </Physics> */}
+
+          <BaseEnvirontment />
+        </Canvas>
+      </Suspense>
     </div>
-  )
-}
-
-const Cube = ({ position }: { position: Vector3 }) => {
-  const ref = useRef(null)
-
-  useFrame(() => {
-    if (!ref.current) return
-    ref.current.rotation.x += 0.02
-    ref.current.rotation.y += 0.02
-    ref.current.rotation.z += 0.02
-    const floorHeight = 3;
-    const amplitude = 1;
-    ref.current.position.y = Math.sin(Date.now() * 0.001) * amplitude + floorHeight;
-  })
-
-  return (
-    <mesh ref={ref} position={position} castShadow receiveShadow>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"#0062ff"} />
-    </mesh>
   )
 }
 
 const BasePlane = () => {
   return (
-    <mesh rotation={[degreeToRadian(-90), 0, 0]} receiveShadow>
-      <planeGeometry args={[100, 100]} />
-      <meshBasicMaterial color={"#70cbff"} />
+    <mesh rotation={[-degreeToRadian(90), 0, 0]} receiveShadow >
+      <planeGeometry args={[300, 300]} />
+      <meshStandardMaterial color={"white"} />
     </mesh>
   )
 }
 
 const BaseEnvirontment = () => {
   return (
-    <Environment background>
-      <mesh>
-        <sphereGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={"#55bffc"} />
+    <Environment background  >
+      <mesh castShadow receiveShadow>
+        <sphereGeometry args={[100, 100, 100]} />
+        <meshBasicMaterial color={baseColor} side={THREE.BackSide} />
       </mesh>
     </Environment>
   )
