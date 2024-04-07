51c1c7ed0d865a8592e99f9b5b72edc90f2547d7
manweljs <manweljs@gmail.com>
2024-04-07 18:18:16 +0800

    Add car animation

--- web/src/components/models/vehicles/VehicleCollection.tsx
+++ web/src/components/models/vehicles/VehicleCollection.tsx
@@ -3,11 +3,13 @@ import { Bus } from './Bus'
 import { Train } from './Train'
 import { AirPlaneBoarding } from './AirPlaneBoarding'
 import { AirPlaneTakeOff } from './AirPlaneTakeOff'
+import { CarBox } from './CarBox'
 
 export default function VehicleCollection() {
     return (
         <group>
             <Bus />
+            <CarBox />
             <Train />
             <AirPlaneBoarding />
             <AirPlaneTakeOff />
